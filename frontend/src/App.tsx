ace07b67533f11c24539e5626328c2eccd1fab21
ana <analeticiadebortoli@gmail.com>
2022-09-08 17:22:48 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from './components/SalesCard/index'
 
 function App() {
   return (
     <>
-      <h1>Oii</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard />
+        </div>
+      </section>
+      </main>
     </>
   )
 }
